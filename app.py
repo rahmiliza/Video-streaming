d7106c0b7ef641e94a6f00f4524b7ad0acd75c2d
rahmiliza <57374297+rahmiliza@users.noreply.github.com>
2025-02-18 13:00:39 +0800

    code update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, Response, request, url_for
 logging.basicConfig()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '\xcd\xe9\xd9\x18\x96A=\xa2\xf7-\xfb\n\xb3\xd3\xb4Uk>8I\x9b\xc4}\xd0'
+app.config['SECRET_KEY'] = ''
 app.config['DEBUG'] = False
 
 @app.route('/')
@@ -13,4 +13,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
